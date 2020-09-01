20758dd41b7037371bd475f2503441b5ebc9445e
cerveau08 <malickcoly342@gmail.com>
2020-09-01 21:48:16 +0000

    init and ok

---
+++ lib/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:login_interface/screens/signInOne.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SignInOne(),
+    );
+  }
+}
